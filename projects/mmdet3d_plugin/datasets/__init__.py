12902f9d9d0adb5725823148378f1a8021d8413d
CuriosiyuWu <Curiosity654@outlook.com>
2025-12-30 22:57:52 +0800

    change dataset file name

--- projects/mmdet3d_plugin/datasets/__init__.py
+++ projects/mmdet3d_plugin/datasets/__init__.py
@@ -1,3 +1,3 @@
 from .custom_nuscenes_dataset import CustomNuScenesDataset
-from .unified_tri3d_dataset import UnifiedMMDet3DDataset
+from .unified_mmdet3d_dataset import UnifiedMMDet3DDataset
 from .pipelines import *
